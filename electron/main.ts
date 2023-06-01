7068c04a3caaf33c669f0e2226f78b9035e9aaf1
majinlei <jkl987321564@163.com>
2023-06-01 15:49:17 +0800

    初始话

---
+++ electron/main.ts
@@ -0,0 +1,183 @@
+// The built directory structure
+//
+// ├─┬ dist
+// │ ├─┬ electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │ ├── index.html
+// │ ├── ...other-static-files-from-public
+// │
+process.env.DIST = join(__dirname, '..')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(process.env.DIST, '../public')
+
+import { join } from 'path'
+import {
+  type MenuItem,
+  type MenuItemConstructorOptions,
+  app,
+  Menu,
+  shell,
+  ipcMain,
+  BrowserWindow
+} from "electron";
+
+let win: BrowserWindow | null
+// Here, you can also use other preload
+const preload = join(__dirname, './preload.js')
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+const url = process.env['VITE_DEV_SERVER_URL']
+const indexHtml = join(process.env.DIST, "index.html");
+// 是否为开发环境
+const isDev = process.env["NODE_ENV"] === "development";
+// 创建菜单
+function createMenu(label = "进入全屏幕") {
+  const menu = Menu.buildFromTemplate(
+    appMenu(label) as (MenuItemConstructorOptions | MenuItem)[]
+  );
+  Menu.setApplicationMenu(menu);
+}
+
+async function createWindow() {
+  win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    minWidth: 1024,
+    minHeight: 768,
+    title: "Main window",
+    // icon: join(process.env.PUBLIC, "favicon.ico"),
+    webPreferences: {
+      preload,
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // electron-vite-vue#298
+    win.loadURL(url);
+    // Open devTool if the app is not packaged
+    // win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+
+  createMenu();
+
+  // Test actively push message to the Electron-Renderer
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
+  // win.webContents.on('will-navigate', (event, url) => { }) #344
+
+  // 窗口进入全屏状态时触发
+  win.on("enter-full-screen", () => {
+    createMenu("退出全屏幕");
+  });
+
+  // 窗口离开全屏状态时触发
+  win.on("leave-full-screen", () => {
+    createMenu();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("second-instance", () => {
+  if (win) {
+    // Focus on the main window if the user tried to open another
+    if (win.isMinimized()) win.restore();
+    win.focus();
+  }
+});
+
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+
+// 菜单栏 https://www.electronjs.org/zh/docs/latest/api/menu-item#%E8%8F%9C%E5%8D%95%E9%A1%B9
+const appMenu = (fullscreenLabel: string) => {
+  const menuItems = [
+    { label: "关于", role: "about" },
+    { label: "开发者工具", role: "toggleDevTools" },
+    { label: "强制刷新", role: "forcereload" },
+    { label: "退出", role: "quit" }
+  ];
+  // 生产环境删除开发者工具菜单
+  if (!isDev) menuItems.splice(1, 1);
+  const template = [
+    {
+      label: app.name,
+      submenu: menuItems
+    },
+    {
+      label: "编辑",
+      submenu: [
+        { label: "撤销", role: "undo" },
+        {
+          label: "重做",
+          role: "redo"
+        },
+        { type: "separator" },
+        { label: "剪切", role: "cut" },
+        { label: "复制", role: "copy" },
+        { label: "粘贴", role: "paste" },
+        { label: "删除", role: "delete" },
+        { label: "全选", role: "selectAll" }
+      ]
+    },
+    {
+      label: "显示",
+      submenu: [
+        { label: "加大", role: "zoomin" },
+        {
+          label: "默认大小",
+          role: "resetzoom"
+        },
+        { label: "缩小", role: "zoomout" },
+        { type: "separator" },
+        {
+          label: fullscreenLabel,
+          role: "togglefullscreen"
+        }
+      ]
+    }
+  ];
+  return template;
+};
+
+// New window example arg: new windows url
+ipcMain.handle("open-win", (_, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
+
